1db5fc31a081bb4f782fd60021e914dd662ef14d
grailedemperor <105563014+grailedemperor@users.noreply.github.com>
2023-01-30 17:47:17 -0800

    non-working models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int a = 20;
+        double b = 3.47;
+        double sum = a + b;
+        String c = "long thick d*ck";
+        String concat = a + c;
+        System.out.println(sum);
+        System.out.println(concat);
+
+    }
+}
